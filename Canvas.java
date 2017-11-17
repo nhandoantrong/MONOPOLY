7314a1bb3b130a5b8cce78f2a828763f99c20437
ITITIU16113 <33054279+ITITIU16113@users.noreply.github.com>
2017-11-17 12:07:01 +0700

    Add files via upload

---
+++ Canvas.java
@@ -0,0 +1,59 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Canvas extends JPanel //implements MouseListener
+	{
+		public final ImageIcon board=new ImageIcon("board1.jpg");
+		public final ImageIcon lulu=new ImageIcon("lulu2.jpg");
+		public ImageIcon[] dice= {new ImageIcon ("1.jpg"),new ImageIcon ("2.jpg"),new ImageIcon ("3.jpg"),
+				new ImageIcon ("4.jpg"),new ImageIcon ("5.jpg"),new ImageIcon ("6.jpg")};
+		public ImageIcon lulugif = new ImageIcon("lulugif.gif");
+		private int x,y,d1,d2;
+		public Canvas(int x,int y,int d1,int d2)
+		{
+			
+			this.x=x;
+			this.y=y;
+			this.d1=d1;
+			this.d2=d2;
+//			addMouseListener(this);
+		}
+		public void setXY(int x,int y)
+		{
+			this.x=x;
+			this.y=y;
+		}
+		public void setDice(int d1,int d2)
+		{
+			this.d1=d1;
+			this.d2=d2;
+		}
+		
+		@Override
+	    public void paintComponent (Graphics g) 
+		 {
+	       super.paintComponent(g);
+	       g.drawImage(board.getImage(),0 ,0 ,null );
+	       g.drawImage(lulu.getImage(), x, y, null);
+	       g.drawImage(lulugif.getImage(),70,70,70,70 ,null );
+	       g.drawImage(dice[d1-1].getImage(),300,300,null);
+	       g.drawImage(dice[d2-1].getImage(),400,300,null);
+	       
+	    }
+//		  @Override
+//		   public void mouseClicked(MouseEvent evt)
+//		  {
+//			  
+//		  }
+//		  @Override public void mousePressed(MouseEvent evt) 
+//		  {
+//			  
+//		  }
+//		   @Override public void mouseReleased(MouseEvent evt) { }
+//		   @Override public void mouseEntered(MouseEvent evt) { }
+//		   @Override public void mouseExited(MouseEvent evt) { }
+	}
